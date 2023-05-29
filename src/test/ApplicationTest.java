6796deacfe3f5bcf32c4ab2295e833531d85a124
sdesiderio <91961553+sdesiderio@users.noreply.github.com>
2023-05-29 23:00:25 +0200

    Update ApplicationTest.java

--- src/test/ApplicationTest.java
+++ src/test/ApplicationTest.java
@@ -24,6 +24,7 @@ public class ApplicationTest {
 		final String dbUrl="jdbc:mysql://localhost:3306/sistemi";
 		final String dbUser="root";
 		final String dbPass="";
+		final String modify = "";
 		
 		Class.forName(dbDriver);
 		
